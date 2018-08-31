2439fcb8c63e963a6df08ac734db24ac058ddb30
Maguicai <maguicai@zjxa.com>
2018-08-31 15:12:55 +0800

    有关链接原理的问题

---
+++ Fundamental_Of_Computer/Part1/chapter_1/example2/main.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2018年08月31日 14时32分05秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+void p1();
+int d = 100;
+int x = 200;
+int main()
+{
+    p1();
+    printf("d=%d,x=%d\n",d,x);
+    return 0;
+}
+
